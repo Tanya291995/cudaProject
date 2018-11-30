42226d50257eb6d58c17c48dd209785e8806c1bd
Tatyana <tatyana29101195@gmail.com>
2018-11-30 09:28:42 +0300

    dd

--- Source.cpp
+++ Source.cpp
@@ -1,15 +1,25 @@
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-
-#include <stdio.h>
-
-//hello world with cuda
-
-__global__ void kernel() {
+#include <GL/glut.h>
+ 
+void displayMe(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+        glVertex3f(0.5, 0.0, 0.5);
+        glVertex3f(0.5, 0.0, 0.0);
+        glVertex3f(0.0, 0.5, 0.0);
+        glVertex3f(0.0, 0.0, 0.5);
+    glEnd();
+    glFlush();
 }
-
-int main() {
-	kernel<<<1,1>>>();
-	printf( "Hello, World!\n" );
-	return 0;
+ 
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(400, 300);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Hello world!");
+    glutDisplayFunc(displayMe);
+    glutMainLoop();
+    return 0;
 }
