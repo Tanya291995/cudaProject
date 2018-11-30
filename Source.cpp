29cd526279374606bcafb6e3b212efe15513772a
Tatyana <tatyana29101195@gmail.com>
2018-11-30 09:25:26 +0300

    g

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,15 @@
-#include <iostream>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 
-using namespace std;
+#include <stdio.h>
 
+//hello world with cuda
 
+__global__ void kernel() {
+}
 
-int main(int argc, char *argv[])
-
-{
-  cout<<"Hello"<<endl;
-  return 0;
+int main() {
+	kernel<<<1,1>>>();
+	printf( "Hello, World!\n" );
+	return 0;
 }
